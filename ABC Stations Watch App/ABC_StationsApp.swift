f7aef975d1eafc0014dc089819adc5e26c6e06b8
sboddeus <sboddeus@gmail.com>
2023-01-07 12:45:43 +1100

    Initial Commit

---
+++ ABC Stations Watch App/ABC_StationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABC_StationsApp.swift
+//  ABC Stations Watch App
+//
+//  Created by Sye Boddeus on 7/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct ABC_Stations_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
